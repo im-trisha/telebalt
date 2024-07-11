3ce5581b7c3f5aaaa431684a5f3e09854891fc7f
unknown <imtrisha>
2024-07-11 08:48:44 +0200

    feat: send error message to admin

--- lib/src/bot/handlers/misc_handlers.dart
+++ lib/src/bot/handlers/misc_handlers.dart
@@ -13,11 +13,19 @@ Future<void> myChatMember(TgContext ctx) async {
 }
 
 Future<void> onError(BotError<TgContext> error) async {
-  error.ctx?.logger.error("""An error has occurred. Here's some info:
-IsMiddleware: ${error.sourceIsMiddleware}
-Update: ${error.ctx?.update.type.name}
-Text (If any): ${error.ctx?.message?.text}
-Error: ${error.error}
-Stacktrace: ${error.stackTrace}
-""");
+  final message = """An error has occurred. Here's some info:
+IsMiddleware: <code>${error.sourceIsMiddleware}</code>
+Update: <code>${error.ctx?.update.type.name}</code>
+Text (If any): <code>${error.ctx?.message?.text}</code>
+Error: <code>${error.error}</code>
+Stacktrace: <code>${error.stackTrace}</code>""";
+  error.ctx?.logger.error(message);
+
+  final chatId = ID.create(error.ctx?.settings.adminId);
+  try {
+    // TODO: Televerse produces an error, add ParseMode as soon as the lib fixes the error
+    await error.ctx?.api.sendMessage(chatId, message);
+  } catch (_, __) {
+    await error.ctx?.api.sendMessage(chatId, "An error has occurred");
+  }
 }
