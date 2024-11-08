e5f860ac6858776299bd7967cb6ffc700f473159
giuseppefn <giuseppe.colucci@etu.unice.fr>
2024-11-08 16:18:37 +0100

    chore: little revamping of providers

--- lib/providers/common.dart
+++ lib/providers/common.dart
@@ -1,15 +1,5 @@
 part of 'providers.dart';
 
-final dioProvider = Provider<Dio>((ref) {
-  final dio = Dio(BaseOptions(
-    baseUrl: ref.watch(settingsProvider).cobaltUrl,
-    validateStatus: (status) => (status ?? 0) < 500,
-  ));
-  ref.onDispose(dio.close);
-
-  return dio;
-});
-
 final settingsProvider = Provider<Settings>((ref) {
   final settings = Settings.fromJson(DotEnv().getDotEnv());
   return settings;
@@ -20,30 +10,40 @@ final loggerProvider = Provider<Talker>((ref) {
   return talker;
 });
 
+final dioProvider = Provider<Dio>((ref) {
+  final dio = Dio(BaseOptions(
+    baseUrl: ref.read(settingsProvider).cobaltUrl,
+    validateStatus: (status) => (status ?? 0) < 500,
+  ));
+
+  ref.onDispose(dio.close);
+  return dio;
+});
+
 final cobaltServiceProvider = Provider<CobaltService>((ref) {
   final cobalt = CobaltService(
-    ref.watch(dioProvider),
-    baseUrl: ref.watch(settingsProvider).cobaltUrl,
+    ref.read(dioProvider),
+    baseUrl: ref.read(settingsProvider).cobaltUrl,
   );
   return cobalt;
 });
 
 final databaseProvider = Provider<Database>((ref) {
-  final founderId = int.tryParse(ref.watch(settingsProvider).adminId);
+  final founderId = int.tryParse(ref.read(settingsProvider).adminId);
   if (founderId == null) {
     ref.read(loggerProvider).error("ADMIN_ID must be an integer.");
     throw Exception("ADMIN_ID must be an integer.");
   }
 
-  final database = Database(ref.watch(settingsProvider).storagePath, founderId);
+  final database = Database(ref.read(settingsProvider).storagePath, founderId);
   ref.onDispose(database.close);
   return database;
 });
 
 final botProvider = Provider<Bot<TgContext>>((ref) {
-  final maybeApi = ref.watch(settingsProvider).apiUrl ?? '';
+  final maybeApi = ref.read(settingsProvider).apiUrl ?? '';
   final botApi = Uri.tryParse(maybeApi)?.toString() ?? RawAPI.defaultBase;
-  final botToken = ref.watch(settingsProvider).botToken;
+  final botToken = ref.read(settingsProvider).botToken;
 
   final bot = Bot<TgContext>(botToken, baseURL: botApi);
   ref.onDispose(bot.stop);
