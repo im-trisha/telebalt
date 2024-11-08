ae0d36a627b0c675f24a63439752d6ab61f8b9f7
giuseppefn <giuseppe.colucci@etu.unice.fr>
2024-11-08 16:06:31 +0100

    chore: edit on error

--- lib/bot/handlers/on_error.dart
+++ lib/bot/handlers/on_error.dart
@@ -1,29 +1,30 @@
 part of '../handlers.dart';
 
 Future<void> onError(BotError<TgContext> error) async {
-  final msg = """An error has occurred. Here's some info:
-IsMiddleware: ```${error.sourceIsMiddleware}```
-Update: ```${error.ctx?.update.type.name}```
-Text (If any): ```${_escapeMDV2(error.ctx?.message?.text)}```
-Error: ```${_escapeMDV2(error.error.toString())}```
-Stacktrace: ```${_escapeMDV2(error.stackTrace.toString())}```""";
-
   if (error.ctx == null) {
-    print(msg);
+    print(error.pretty());
     return;
   }
 
-  error.ctx?.logger.error(msg);
+  error.ctx?.logger.error(error.pretty());
 
-  final to = ID.create(error.ctx?.settings.adminId);
   try {
-    await error.ctx?.api.sendMessage(to, msg, parseMode: ParseMode.markdownV2);
+    final user = await error.ctx?.user();
+    if (error.ctx?.inlineQuery != null || user == null) return;
+
+    await error.ctx?.reply(error.ctx!.t(user).errors.generic);
   } catch (e) {
-    await error.ctx?.api.sendMessage(to, "An error has occurred");
+    // Ignore error
   }
 }
 
-String? _escapeMDV2(String? input) {
-  K.mdv2Characters.forEach((k, v) => input = input?.replaceAll(k, v));
-  return input;
+extension on BotError<TgContext> {
+  String pretty() {
+    return """An error has occurred. Here's some info:
+IsMiddleware: $sourceIsMiddleware
+Update: ${ctx?.update.type.name}
+Text (If any): ${ctx?.message?.text}
+Error: ${toString()}
+Stacktrace: ${stackTrace.toString()}""";
+  }
 }
